818951ace72250bc16160722591928ff70ba252f
Manabu Nakazawa <mshibanami+git@gmail.com>
2025-09-24 18:22:17 +1000

    Add a test case for omitting HTML comments

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -54,4 +54,12 @@ describe('markdown-it-sanitize-html', () => {
     const actualHtml = md.render(unsafeMarkdown);
     expect(actualHtml).toBe(expectedHtml);
   });
+
+  it('should not preserve HTML comments', () => {
+    const md = markdownIt({ html: true }).use(markdownItSanitizeHtml);
+    const unsafeMarkdown = 'Text before <!-- This is a comment --> Text after';
+    const expectedHtml = '<p>Text before  Text after</p>\n';
+    const actualHtml = md.render(unsafeMarkdown);
+    expect(actualHtml).toBe(expectedHtml);
+  });
 });
